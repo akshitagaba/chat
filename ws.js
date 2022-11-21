def9b6984725c4536106fc0fe34c9673d2e267a9
akshitagaba <86964390+akshitagaba@users.noreply.github.com>
2022-11-21 13:28:09 +0530

    Add files via upload

---
+++ ws.js
@@ -0,0 +1,25 @@
+const{WebSocketServer, WebSocket} = require('ws');
+
+const wss = new WebSocketServer({port : 8080});
+
+wss.on('connection', (ws) => {
+    console.log('new client connected...');
+
+    ws.on('message',(data)=>{
+        console.log(JSON.parse(data));
+        wss.clients.forEach((client) => {
+            if(client!== ws && client.readyState === WebSocket.OPEN){
+                 client.send(JSON.stringify(JSON.parse(data)));
+            }
+        })
+    })
+
+   ws.on('close', ()=> {
+      console.log('client has disconnected'); 
+   })
+
+   ws.onerror = function () {
+    console.log('some error occured');
+   }
+
+})
